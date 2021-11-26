3868cfecc9b175ec187d204c89fcd480fd21e0ec
licheng2 <licheng2@zto.com>
2021-11-26 13:08:42 +0800

    xlogs 自定义封装

---
+++ go.mod
@@ -0,0 +1,5 @@
+module xlogs
+
+go 1.16
+
+require go.uber.org/zap v1.19.1 // indirect
---
+++ rotate.go
@@ -0,0 +1,19 @@
+package xlogs
+
+import (
+	"io"
+	"xlogs/rotate"
+)
+
+func newRotate(config *LoggerConfig) io.Writer {
+	rotateLog := rotate.NewLogger()
+	rotateLog.Filename = config.Filename()
+	rotateLog.MaxSize = config.MaxSize
+	rotateLog.MaxAge = config.MaxAge
+	rotateLog.MaxBackups = config.MaxBackup
+	rotateLog.Interval = config.Interval
+	rotateLog.LocalTime = true
+	rotateLog.Compress = config.Compress
+
+	return rotateLog
+}
